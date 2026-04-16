888cc1602a236b26595fe58696a07d9b70378515
Juan Manuel Martinez Caamaño <jmartinezcaamao@gmail.com>
2026-04-16 11:11:25 +0200

    Review: no-auto, use Expected<bool>

--- llvm/lib/Debuginfod/Debuginfod.cpp
+++ llvm/lib/Debuginfod/Debuginfod.cpp
@@ -321,7 +321,8 @@ Expected<std::string> getCachedOrDownloadArtifact(
     if (!PruningPolicyOrErr)
       return PruningPolicyOrErr.takeError();
 
-    auto ErrOrPruned = pruneCache(CacheDirectoryPath, *PruningPolicyOrErr);
+    Expected<bool> ErrOrPruned =
+        pruneCache(CacheDirectoryPath, *PruningPolicyOrErr);
     if (!ErrOrPruned)
       return ErrOrPruned.takeError();
 
